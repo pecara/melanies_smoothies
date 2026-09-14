a0ba4a2e8f2be3ab64f43c3aba7c66955b29cb56
pecara <anze.pecar@in516ht.com>
2026-09-14 08:36:58 +0200

    Fix dataframe variable name for conversion to pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests  
+import pandas
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -17,7 +18,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 #convert the snowpark dataframe to a pandas dataframe so we can use the loc function
-pd_df=my_dataFrame.to_pandas()
+pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
