56549ef3adb8fc6d9ce7ca893ec52dae789c3527
pecara <anze.pecar@in516ht.com>
2026-09-14 08:30:11 +0200

    Refactor dataframe handling and ingredient selection
    Comment out dataframe display and stop calls, and convert Snowpark dataframe to pandas for further processing.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,13 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+#convert the snowpark dataframe to a pandas dataframe so we can use the loc function
+pd_df=my_dataFrame.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -27,6 +32,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value ', fruit_Chosen, ' is ', search_on, '.')
+      
         st.subheader(fruit_chosen+ ' Nutriotion Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
